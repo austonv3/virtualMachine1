8100eb355e09c4f04e5bf20902fdd4c45019b47c
Auston <austonv@bellsouth.net>
2025-07-08 16:21:51 -0400

    StackTest Setup

---
+++ VirtualMachine1.py
@@ -0,0 +1,115 @@
+''' Current code: StackTest
+// Executes a sequence of arithmetic and logical operations on the stack.
+
+push constant 17
+push constant 17
+eq
+push constant 17
+push constant 16
+eq
+push constant 16
+push constant 17
+eq
+push constant 892
+push constant 891
+lt
+push constant 891
+push constant 892
+lt
+push constant 891
+push constant 891
+lt
+push constant 32767
+push constant 32766
+gt
+push constant 32766
+push constant 32767
+gt
+push constant 32766
+push constant 32766
+gt
+push constant 57
+push constant 31
+push constant 53
+add
+push constant 112
+sub
+neg
+and
+push constant 82
+or
+not
+'''
+
+def Parser(line):
+    commandType = str()
+    arg1 = str()
+    arg2 = str()
+    if len(line) > 1 and line[0] != '/':
+        arguments = line[:-1].split(' ')
+        if arguments[0] == 'push':
+            commandType = 'C_PUSH'
+            arg1 = arguments[1]
+            arg2 = arguments[2]
+        else:
+            commandType = 'C_ARITHMETIC'
+            arg1 = arguments[0]
+        print(commandType, arg1, arg2)
+
+    return commandType, arg1, arg2
+
+def CodeWriter(command):
+
+    output = ''
+
+    if command[0] == 'C_PUSH':
+        segment = command[1] #doesn't actually need to do anything when using the constant segment. Might need some extra logic for that in the future...
+        index = command[2]
+        output += '@%s\n' \
+        'D=A\n' \
+        '@SP\n' \
+        'A=M\n' \
+        'M=D\n' \
+        '@SP\n' \
+        'M=M+1\n' % index
+
+    if command[0] == 'C_ARITHMETIC': #I think this one here is right.
+        output += '@SP\n' \
+        'M=M-1\n' \
+        'A=M\n' \
+        'D=M\n' \
+        '@SP\n' \
+        'M=M-1\n' \
+        'A=M\n' \
+        'A=M\n' \
+        'D=D+A\n' \
+        '@SP\n' \
+        'A=M\n' \
+        'M=D\n' \
+        '@SP\n' \
+        'M=M+1\n'
+
+    return output
+
+def EndCode():
+    end = '(END)\n' \
+    '@END\n' \
+    '0;JMP\n'
+    return end
+
+def VMTranslator():
+    filename = input("Please input file name without extension: ")
+    output = ''
+    with open(filename + ".vm", 'r') as bytecode:
+        with open(filename + ".asm", 'w') as machineCode:
+            for line in bytecode:
+                command = Parser(line)
+                if command:
+                    output = CodeWriter(command)
+                if output:
+                    machineCode.write(output)
+            end = EndCode()
+            machineCode.write(end)
+    return
+
+VMTranslator()
